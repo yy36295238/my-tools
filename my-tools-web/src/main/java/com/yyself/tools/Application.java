c471c66acf45226806b01140564e8e7751fdad94
yangyu <yangyu@mintq.com>
2020-05-21 19:19:28 +0800

    [feat]kotmybatis在线生成工具

--- my-tools-web/src/main/java/com/yyself/tools/Application.java
+++ my-tools-web/src/main/java/com/yyself/tools/Application.java
@@ -3,7 +3,11 @@ package com.yyself.tools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -31,4 +35,13 @@ public class Application {
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
     }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> {
+            ErrorPage errorPage = new ErrorPage(HttpStatus.NOT_FOUND, "/index.html");
+            factory.addErrorPages(errorPage);
+        };
+    }
+
 }
